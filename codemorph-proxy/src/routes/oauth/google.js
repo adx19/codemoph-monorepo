424057ab6165ae022910b1575bce13ad5c12ae00
Abhijit P <pabhijit047@gmail.com>
2026-01-07 20:05:03 +0530

    Fixing the OAuth

--- codemorph-proxy/src/routes/oauth/google.js
+++ codemorph-proxy/src/routes/oauth/google.js
@@ -8,7 +8,7 @@ const router = express.Router();
 
 router.get("/google", (req, res) => {
   const redirect =
-    "https://accounts.google.com/o/oauth2/v2/auth?" +
+    `https://accounts.google.com/o/oauth2/v2/auth?` +
     new URLSearchParams({
       client_id: process.env.GOOGLE_CLIENT_ID,
       redirect_uri: process.env.GOOGLE_OAUTH_REDIRECT,
