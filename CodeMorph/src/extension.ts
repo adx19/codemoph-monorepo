9117d4fd695047d1ded36d2009da78a79f5ab2e1
Abhijit P <pabhijit047@gmail.com>
2026-01-09 11:48:00 +0530

    Fixing prompt issue

--- CodeMorph/src/extension.ts
+++ CodeMorph/src/extension.ts
@@ -196,6 +196,7 @@ async function convertWithAI(
 Convert the following ${from} code to ${to}.
 ${withComments ? "Add comments." : "Do NOT add comments."}
 Return ONLY valid ${to} code.
+Do not change the class name or variable names unless absolutely necessary.
 
 CODE:
 ${code}
