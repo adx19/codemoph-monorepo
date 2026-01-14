cf591505453eb71aeb490cae0140288819500513
Abhijit P <pabhijit047@gmail.com>
2026-01-14 11:31:19 +0530

    Changed back to free tier of gemini

--- codemorph-proxy/src/routes/convert.js
+++ codemorph-proxy/src/routes/convert.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { pool } from "../db.js";
-import { generateResponse } from "../utils/huggingface.js";
+import { generateResponse } from "../utils/gemini.js";
 import { apiKeyMiddleware } from "../auth.js";
 import crypto from "crypto";
 
